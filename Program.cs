6c66fb83fcd635fee273f9d47b2d3d7ac11ae985
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-28 01:48:10 +0500

    zadacha6_1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+using System;
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числа:  ");
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > 0)
+            { 
+                count++;
+            }
+        }
+   Console.WriteLine(count);
+    }
+}
